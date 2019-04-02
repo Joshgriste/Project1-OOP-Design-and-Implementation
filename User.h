f2ef077e2d70f007742ecc2bdbf18fb08aed81fb
Npowe3 <33768301+Npowe3@users.noreply.github.com>
2019-04-02 12:26:32 -0400

    Update User.h
    Added description of access types and setAccess constructor

--- User.h
+++ User.h
@@ -12,6 +12,13 @@ class User{
 		void setPWD(string);
 		void setName(string, string);
 		void setID(string);
+		/*access type set to ATM by default in .cpp file
+		Access types:
+			Audit - can change account information and add users
+			Teller - can move money to/from accounts
+			ATM - can withdraw money and view account info
+		*/
+		void setAccess(string);
 		string getName();
 		string getID();
 		virtual void print();
