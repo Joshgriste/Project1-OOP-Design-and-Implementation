349119e882e2a50f4cf5b2f29032160d4b95c480
Npowe3 <33768301+Npowe3@users.noreply.github.com>
2019-04-02 12:17:04 -0400

    Update User.h
    Filled in user header file to contain the basic required information

--- User.h
+++ User.h
@@ -1,1 +1,27 @@
 //User.h
+#include <iostream>
+using namespace std;
+
+#ifndef USER_H
+#define USER_H
+
+class User{
+	public:
+		User();
+		void setUName(string);
+		void setPWD(string);
+		void setName(string, string);
+		void setID(string);
+		string getName();
+		string getID();
+		virtual void print();
+	protected:
+		string first;
+		string last;
+		string id;
+		string UName;
+		string PWD;
+		
+};
+
+#endif
