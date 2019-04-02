900da1265f82e48caf5cdef089db88aa32bcbd74
Npowe3 <33768301+Npowe3@users.noreply.github.com>
2019-04-02 12:32:00 -0400

    Update Admin.h
    Filled in header file with print function and Access function

--- Admin.h
+++ Admin.h
@@ -1,1 +1,20 @@
 //Admin.h
+#include "User.h"
+
+#ifndef ADMIN_H
+#define ADMIN_H
+
+class Admin{
+	public:
+		Admin();
+		/*access type set to ATM by default in user
+		Access types:
+			Audit - can change account information and add users
+			Teller - can move money to/from accounts
+			ATM - can withdraw money and view account info
+		*/
+		void setAccess(string);
+    void print();		
+};
+
+#endif
