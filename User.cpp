c8b63c6f91a96872de92c44393c81aa0a92af4ee
Npowe3 <33768301+Npowe3@users.noreply.github.com>
2019-04-04 11:31:04 -0400

    Update User.cpp
    Completed constructors

--- User.cpp
+++ User.cpp
@@ -1,1 +1,37 @@
 //User.cpp
+
+#include "User.h"
+
+User::User(){
+  setName("John", "Doe");
+	setUName("Default");
+	setPWD("Passwd");
+	setID("0000000");
+  setAccess("ATM");
+}
+void User::setName(string f, string l){
+  first = f;
+  last = l;
+}
+void User::setUName(string u){
+	UName = r;
+}
+
+void User::setPWD(string p){
+	PWD = p;
+}
+
+void User::setID(string i){
+	id = i;
+}
+void User::setAccess(string a){
+  Access = a; 
+}
+void User::print(){
+	cout<< "User Information\n"
+		<< "Full Name: "<< first<< " "<< last<< "\n"
+		<< "Username: "<< UName<< "\n"
+		<< "ID: "<< id<< "\n"
+		<< "Password "<< PWD<< "\n"
+		<< "Access Type"<< access<< endl;
+}
