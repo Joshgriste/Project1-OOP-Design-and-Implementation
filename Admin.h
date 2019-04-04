7c1b0a111d63cd5fc9e539a08ee3f295434f4d54
Michael Pacholok <mepacholok@captechu.edu>
2019-04-04 11:15:49 -0400

    Made the base header files

--- Admin.h
+++ Admin.h
@@ -1,1 +1,14 @@
 //Admin.h
+#include <iostream>
+using namespace std;
+
+#ifndef ADMIN_H
+#define ADMIN_H
+
+class Customer: public ADMIN{
+	public:
+		
+		
+};
+#endif
+	
--- Customer.h
+++ Customer.h
@@ -1,1 +1,14 @@
 //Customer.h
+#include <iostream>
+using namespace std;
+
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+class Customer: public User{
+	public:
+		
+		
+};
+#endif
+	
