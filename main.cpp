d2c06bc9e97fb9a9f701e6071ef6f9a6b67935d6
mpatchs <48941601+mpatchs@users.noreply.github.com>
2019-04-09 12:24:27 -0400

    Update Main.cpp
    Added the file decryption so we can can grab users from the file and log them in if there information is correct

--- main.cpp
+++ main.cpp
@@ -1,31 +1,76 @@
 //main.cpp
 #include <iostream>
 #include <bits/stdc++.h>
-#include <string>>
+#include <string>
 
 using namespace std;
 
-int verifycreds()
+  
+//Function that will take an encrypted string and decrypt it (returns as a string)
+string Decrypt(string usernameinput, string passinput)
 {
+	char key = 'J'; //XOR key encryption
+	ifstream is("CustomerInfo.txt"); //opens file
+    string str = "Placeholder";
+    string decrypted;
+    
+    int i =0;
+       if(!is.is_open()) { //error if file fails to open
+     	 perror("Error open");
+     	 exit(EXIT_FAILURE);
+   		}
+   try {
+    	while(!is.eof()) { // while not at the end of the file
+    		getline(is, str); //grabs the line
+    		decrypted = str;	
+			for (int i = 0; i < str.size(); i++) //decrypts line from file
+				 decrypted[i] = str[i] ^ key;
+			auto string name = decrypted.substr(0, decrypted.find(' ')); //grabs names
+			auto string token = decrypted.substr(decrypted.find(' ', name.length()) + 1); //break second half of string up
+			auto string password = token.substr(0, token.find(' ')); //grabs password
+			auto string id = token.substr(token.find(' ', password.length())); //grabs user id
+			id = id.substr(1); //removes spaces at front of string
+			if (name == usernameinput && password == passinput){ // checks if user input equals the stored info
+				cout << "User Logged In!\n";
+				int auth;
+				istringstream (id) >> auth; //change id from string to int
+				switch(auth){ //switch based on ID
+					case 1:
+						cout << "user menu goes here";
+						break;
+					case 2:
+						cout << "banker menu goes here";
+						break;
+					case 3:
+						cout << "admin menu goes here";
+						break;
+				}
+			}
+	}
+	}catch(const std::out_of_range& e){ // catches error showing that we reached the end of the file meaning user not found
+		cout << "User Not Found\n";
+		return "not found";
+	}
+}
+
+int verifylogin()
+{ //user enters username and password
 	string username;
 	string password;
 	cout << "Username: ";
 	cin >> username;
 	cout << "Password: ";
 	cin >> password;
-	
+	Decrypt(username, password);
 	
 }
-
-
 
 int main()
 {
 	int userauth = 0;
 	cout << "Welcome to the banking system!\n"
 		 << "Please Login:\n";
-	userauth = verifycreds();
-	
-	
-	
+	while(1){ //runs loop nonstop
+	userauth = verifylogin(); //grabs user info
+	}
 }
