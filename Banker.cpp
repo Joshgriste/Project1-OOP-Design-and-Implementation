fbb34a99169de5f7874191344f8cf5123cee387c
jhanyok <48803966+jhanyok@users.noreply.github.com>
2019-04-10 16:45:54 -0400

    Update Banker.cpp
    Added the Banker menu, but still trying to make it work

--- Banker.cpp
+++ Banker.cpp
@@ -17,3 +17,19 @@ void Banker::print(){
 		<< "Password "<< PWD<< "\n"
 		<< "Access Type"<< access<< endl; 
 }
+
+void Banker::displayMenu()
+{
+	int userAuth;
+	if(userAuth == 2)
+	{
+		
+		int choice;	
+		cout<<"Banker Menu\n"
+		<<"1.Enter a new client\n"
+		<<"2.Modify an existing client\n"
+		<<"3.Exit\n"
+		<<"Enter your choice: ";
+		cin >>choice;
+	}
+}
